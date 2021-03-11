5652ad446f594734cf417466655a52f52b0a925b
Ark <lightsky1042@gmail.com>
2021-03-11 18:02:48 +0800

    完成首版卡片样式

--- src/components/content.vue
+++ src/components/content.vue
@@ -3,20 +3,27 @@
     <div class="card">
       <div class="card-group">
         <img class="cardimg" :src='$store.state.background_card'>
+
         <div class="cardimg-info">
           <div class="cardimg-tittle">Windows 10X延期发布，微软这次做系统能学乖吗？</div>
-          <div>这两款新品都是主打双屏概念，只不过Surface Duo采用的是系统......</div>
+          <div class="cardimg-subtittle">这两款新品都是主打双屏概念，只不过Surface Duo采用的是系统......</div>
         </div>
 
       </div>
 
       <div class="card-infomation">
+        <div class="card-infomation-tittle">
+          <div class="infomation-tittle">麓思实验室</div>
+          <div class="infomation-subtittle">技术让生活更有趣</div>
+
+        </div>
+
         <vue-qr class="qrcode"  :text="$store.state.text_QR" :size="80" :margin="6"></vue-qr>
 
       </div>
 
       <div class="card-foot">
-      CardPoster黑体
+      CardPoster
 
 
       </div>
@@ -91,6 +98,8 @@ export default {
   margin: auto;/*重要*/
 
 }
+
+
 .cardimg-info{
   position: absolute;/*重要*/ /*子类绝对布局必填*/
 
@@ -103,10 +112,16 @@ export default {
   font-family: SourceHanSansCN;
   color: white;
   font-size: 20px;
-  text-shadow:2px 2px 8px #afafaf;
+  text-shadow:2px 2px 2px #afafaf;
+  letter-spacing: 1px;
 }
 .cardimg-subtittle{
-
+  font-family: 江城圆体;
+  margin-top: 12px;
+  font-size: 14px;
+  color: white;
+  text-shadow:1px 1px 2px #686868;
+  letter-spacing: 1.5px;
 }
 
 .qrcode{
@@ -122,10 +137,30 @@ export default {
   flex-direction : row;
 
 }
+.card-infomation-tittle{
+  display: flex;
+  display: inline-flex;
+  flex-direction : column;
+  /*margin-top: 18px;*/
+  margin-left: 60px;
+  justify-content:center;
+  height: 80px;
+}
+.infomation-tittle{
+  font-family: SourceHanSansCN;
+  font-size: 20px;
+}
+.infomation-subtittle{
+  font-family: 江城圆体;
+  font-size: 12px;
+  color: #bfbfbf;
+  margin-top: 6px;
+
+}
 
 .card-foot{
-  font-family: SourceHanSansCN;
-  color: #bfbfbf;
+  font-family: 江城圆体;
+  color: #e2e2e2;
   margin-top: 12px;
 
 }
